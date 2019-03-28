0b1517bace670be10f2a5b5259f418ac68e36205
fanny <fannykatusse@gmail.com>
2019-03-28 09:39:10 +0100

    add some functions

---
+++ ft_tabcopy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_tabcopy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fcatusse <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/21 12:29:02 by fcatusse          #+#    #+#             */
+/*   Updated: 2019/03/22 18:32:28 by fcatusse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char		**ft_tabcopy(char **tab_dest, char **tab)
+{
+	int		i;
+
+	i = -1;
+	if (!(tab_dest = malloc(sizeof(char *) * ft_tablen(tab))))
+		return (NULL);
+	while (tab && tab[++i])
+		if (!(tab_dest[i] = ft_strdup(tab[i])))
+		{
+			ft_tabfree(tab_dest);
+			return (NULL);
+		}
+	tab_dest[i] = 0;
+	return (tab_dest);
+}
---
+++ realloc_tab.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   realloc_tab.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fcatusse <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/25 13:45:15 by fcatusse          #+#    #+#             */
+/*   Updated: 2019/03/25 13:50:49 by fcatusse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char		**realloc_tab(char **tab, int len)
+{
+	int		i;
+	char	**new;
+
+	i = -1;
+	if (!tab)
+		return (NULL);
+	if (!(new = (char **)malloc(sizeof(*new) * (len + 1))))
+		return (NULL);
+	while (tab && tab[++i] && i < len)
+		new[i] = ft_strdup(tab[i]);
+	ft_tabfree(tab);
+	return (new);
+}
